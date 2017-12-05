04e5d9eb8440c86f4d31463f0b631f74943f2411
klaney <keatonus@gmail.com>
2017-12-05 16:03:25 -0700

    added gulp task to clean and compile the typescript next is a watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,24 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
+var del = require("del");
+var util = require("util");
 
-gulp.task("default", function () {
+gulp.task("clean", function(cb) {
+    return del([
+        'dist'
+    ])
+})
+
+gulp.task("compile", function (cb) {
     return tsProject.src()
         .pipe(tsProject())
         .js.pipe(gulp.dest("dist"));
 });
+
+gulp.task("build", [
+    "clean",
+    "compile"
+], function() {
+    util.log('default task executed.');
+  })
