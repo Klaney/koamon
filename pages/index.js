4e222a3af4dbdb029dd0c79397e2a8bb775f4278
klaney <keatonus@gmail.com>
2018-02-07 20:47:33 -0700

    pokeapi is garbage and the cors policy sucks, had to hack past it

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,40 @@
-export default () => <div>Welcome to next.js!</div>;
+let f = require("isomorphic-fetch");
+let P = require("pokeapi-js-wrapper");
+// let Pokedex = new P.Pokedex();
+let myInit = {
+  method: "GET",
+  cache: "default"
+};
+
+async function getPokemon(name) {
+  const response = await f("http://pokeapi.co/api/v2/pokemon/1", myInit);
+  const body = await response.json();
+  // console.log(body);
+  return body;
+}
+
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { pokemon: {} };
+  }
+  async componentDidMount() {
+    const response = await fetch(
+      "https://cors.now.sh/https://pokeapi.co/api/v2/pokemon/1",
+      myInit
+    );
+    console.log(response);
+    const body = await response.json();
+    this.setState({ pokemon: body });
+  }
+  render() {
+    return (
+      <div>
+        <h1>hello world</h1>
+        <pre>{JSON.stringify(this.state.pokemon.name, null, 2)}</pre>
+      </div>
+    );
+  }
+}
+
+export default Page;
